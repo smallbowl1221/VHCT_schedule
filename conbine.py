a381bab9150e85dc70ee2820d4b9cffcc314b84d
smallbowl <ken91234@gmail.com>
2023-08-04 16:52:14 +0800

    develope to date function

--- conbine.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-date_before = pd.read_csv(os.path.join("Data","calendar",os.listdir('Data/calendar')[1]) ,encoding='big5')
-date_df=pd.read_csv(os.path.join("Data","calendar",os.listdir('Data/calendar')[0]) ,encoding='big5')
-date_after = pd.read_csv(os.path.join("Data","calendar",os.listdir('Data/calendar')[2]) ,encoding='big5')
-
-date_df = pd.concat([date_before.iloc[-7:],date_df,date_after.iloc[:7]])
-date_df.reset_index(drop=True,inplace=True)
